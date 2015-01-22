a98c492b5dd77573e4efd1dccb2ce6bbf39cd0a6
nacho <nacho@limenius.com>
2015-01-22 17:41:39 +0100

    Authorization in exchanges

--- src/msg_exchange_serv.erl
+++ src/msg_exchange_serv.erl
@@ -2,13 +2,12 @@
 
 -behaviour(gen_server).
 
--export([init/1, terminate/2, code_change/3, handle_call/3,
-         handle_cast/2, handle_info/2]).
 -export([start/1]).
 -export([stop/1]).
+-export([init/1, terminate/2, code_change/3, handle_call/3,
+         handle_cast/2, handle_info/2]).
 
-
--record(state, {exchange, broker, auth_config}).
+-record(state, {exchange, exchange_name, broker, auth_config, already_auth}).
 
 
 start(Exchange) ->
@@ -23,14 +22,23 @@ init([ExchangeName]) ->
     {ok, Exchange} = apply(BrokerModule, start_exchange, [Broker]),
     ok = apply(BrokerModule, declare_exchange, [Exchange, {ExchangeName, <<"faonout">>}]),
     {ok, AuthConfig} = application:get_env(carotene, publish_auth),
-    {ok, #state{exchange = Exchange, broker = Broker, auth_config = AuthConfig}}.
+    {ok, #state{exchange = Exchange, exchange_name = ExchangeName, broker = Broker, auth_config = AuthConfig, already_auth = false}}.
 
 handle_info(shutdown, State) ->
     {stop, normal, State}.
 
-handle_call({send, Message, UserId}, _From, State = #state{exchange = Exchange}) ->
-    ok = gen_server:call(Exchange, {publish,  Message}),
-    {reply, ok, State};
+handle_call({send, Message, UserId}, _From, State = #state{exchange = Exchange, exchange_name = ExchangeName, auth_config = AuthConfig}) ->
+    case already_auth of
+        true -> ok = gen_server:call(Exchange, {publish,  Message}),
+                {reply, ok, State};
+        _ ->
+            case can_publish(UserId, AuthConfig, ExchangeName) of
+                ok ->
+                    ok = gen_server:call(Exchange, {publish,  Message}),
+                    {reply, ok, State#state{already_auth = true}};
+                Error -> {reply, {error, Error}, State}
+            end
+    end;
 
 handle_call(stop, _From, State) ->
     {stop, normal, ok, State}.
@@ -43,3 +51,39 @@ terminate(_Reason, _State) ->
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
+
+%% Internal
+can_publish(UserId, AuthConfig, ExchangeName) ->
+    case lists:keyfind(enabled, 1, AuthConfig) of
+        false -> ok;
+        {enabled, false} -> ok;
+        {enabled, true} -> case lists:keyfind(level, 1, AuthConfig) of
+                               false -> bad_configuration;
+                               {level, anonymous} -> ok;
+                               {level, auth} -> case UserId of
+                                                    undefined -> needs_authentication;
+                                                    _ -> ok
+                                                end;
+                               {level, ask} -> case ask_authentication(UserId, AuthConfig, ExchangeName) of
+                                                   true -> ok;
+                                                   Error -> Error
+                                               end
+                           end;
+        _ -> ok
+    end.
+
+ask_authentication(UserId, AuthConfig, ExchangeName) ->
+    case lists:keyfind(authorization_url, 1, AuthConfig) of
+        false -> bad_configuration;
+        {authorization_url, AuthorizeUrl} ->
+            {ok, {{_Version, 200, _ReasonPhrase}, _Headers, Body}} = httpc:request(post, {AuthorizeUrl, [], "application/x-www-form-urlencoded", "user_id="++binary_to_list(UserId)++"&exchange="++binary_to_list(ExchangeName)}, [], []),
+            case jsx:decode(binary:list_to_bin(Body)) of
+                [{<<"authorized">>, <<"true">>}] -> true;
+                [{<<"authorized">>, <<"false">>}] -> no_authorization;
+                Tell -> 
+                    bad_server_response_on_authorization
+            end
+    end.
+
+
+
--- test/exchange_tests.erl
+++ test/exchange_tests.erl
@@ -7,7 +7,91 @@ start_test_() ->
               [start_and_test_running()]
       }.
 
-start_and_test_running() ->
+publish_anonymous_authlevel_test_() ->
+    {"Can't publish if I am anonymous and level is auth",
+     {setup, 
+        fun start_with_config_level_auth/0,
+        fun stop/1,
+        fun(ExchangeServ) ->
+                [try_send_and_needs_auth(ExchangeServ)]
+        end
+     }}.
+
+publish_anonymous_asklevel_test_() ->
+    {"Can't publish if I am anonymous asking is false",
+     {setup, 
+        fun start_with_config_level_ask/0,
+        fun stop/1,
+        fun(ExchangeServ) ->
+                [try_send_and_auth_wrong(ExchangeServ)]
+        end
+     }}.
+
+%% Helpers
+set_config_level_auth() ->
+    application:set_env(carotene, publish_auth, [{enabled, true},
+                                                 {level, auth}
+                                                ]).
+
+set_config_level_ask() ->
+    application:set_env(carotene, publish_auth, [{enabled, true},
+                                                 {authorization_url, "http://example.com"},
+                                                 {level, ask}
+                                                ]).
+
+start() ->
+    meck:new(broker_sup),
+    meck:expect(broker_sup, get_broker, fun() ->
+                                                {broker_module_mock, broker_mock} end),
+    meck:new(broker_module_mock, [non_strict]),
+    meck:expect(broker_module_mock, start_exchange, fun(_Broker) ->
+                                                            {ok, some_exchange} end),
+    meck:expect(broker_module_mock, declare_exchange, fun(_Exchange, _ExchangeSpecs) ->
+                                                              ok end),
     Res = msg_exchange_serv:start(<<"room1">>),
-    ?debugFmt("my result~p~n", [Res]),
+    
+    meck:unload(broker_sup),
+    meck:unload(broker_module_mock),
+    {ok, Pid} = Res,
+    Pid.
+
+%% Setups/Teardowns
+start_with_config_level_auth() ->
+    set_config_level_auth(),
+    start().
+
+start_with_config_level_ask() ->
+    set_config_level_ask(),
+    start().
+
+stop(ExchangeServ) ->
+    msg_exchange_serv:stop(ExchangeServ).
+
+%% Tests
+start_and_test_running() ->
+    meck:new(broker_sup),
+    meck:expect(broker_sup, get_broker, fun() ->
+                                                {broker_module_mock, broker_mock} end),
+    meck:new(broker_module_mock, [non_strict]),
+    meck:expect(broker_module_mock, start_exchange, fun(_Broker) ->
+                                                            {ok, some_broker_exchange} end),
+    meck:expect(broker_module_mock, declare_exchange, fun(_Exchange, _ExchangeSpecs) ->
+                                                              ok end),
+    application:set_env(carotene, publish_auth, nothing),
+    Res = msg_exchange_serv:start(<<"room1">>),
+    meck:unload(broker_sup),
+    meck:unload(broker_module_mock),
     ?_assertMatch({ok, _}, Res).
+
+try_send_and_needs_auth(ExchangeServ) ->
+    Res = gen_server:call(ExchangeServ, {send, <<"hi">>, undefined}),
+    ?_assertEqual({error, needs_authentication}, Res).
+
+try_send_and_auth_wrong(ExchangeServ) ->
+    meck:new(httpc),
+    meck:expect(httpc, request, fun(post, _, _, _) ->
+                                        {ok, {{v, 200, rp}, h, binary:bin_to_list(jsx:encode([{<<"authorized">>, <<"false">>}]))}} end),
+    Res = gen_server:call(ExchangeServ, {send, <<"hi">>, <<"user1">>}),
+    meck:unload(httpc),
+    ?_assertEqual({error, no_authorization}, Res).
+    
