09c38243b7779c7ed7e4933daa0cb7bff5038587
nacho <nacho@limenius.com>
2015-02-26 17:59:47 +0100

    removed unnecesary features

--- src/api_channels_handler.erl
+++ src/api_channels_handler.erl
@@ -3,10 +3,7 @@
 -export([init/2]).
 -export([allowed_methods/2]).
 -export([content_types_provided/2]).
--export([content_types_accepted/2]).
 -export([resource_exists/2]).
--export([from_json/2, subscribe_channel/2, subscribe_channel/3]).
-
 
 -export([channel_to_json/2]).
 
@@ -14,52 +11,12 @@ init(Req, Opts) ->
     {cowboy_rest, Req, Opts}.
 
 allowed_methods(Req, State) ->
-    {[<<"GET">>, <<"POST">>], Req, State}.
+    {[<<"GET">>], Req, State}.
 
 content_types_provided(Req, State) ->
     {[
       {{<<"application">>, <<"json">>, []}, channel_to_json}
      ], Req, State}.
-
-content_types_accepted(Req, State) ->
-    {[
-      {{<<"application">>, <<"json">>, []}, from_json}
-    ], Req, State}.
-
-from_json(Req, State) ->
-    case cowboy_req:binding(channel, Req) of
-        undefined ->
-            subscribe_channel(Req, State);
-        Channel ->
-            subscribe_channel(Channel, Req, State)
-    end.
-
-subscribe_channel(Channel, Req, State) ->
-    ok = gen_server:call(admin_serv, {subscribe, {channel, Channel}}),
-
-    case cowboy_req:method(Req) of
-        <<"POST">> ->
-            {{true, Channel}, Req, State};
-        <<"PUT">> ->
-            {{true, Channel}, Req, State};
-        _ ->
-            {true, Req, State}
-    end.
-
-subscribe_channel(Req, State) ->
-    {ok, PostParams, Req2} = cowboy_req:body_qs(Req),
-    {_, Channel} = lists:keyfind(<<"channel">>, 1, PostParams),
-
-    ok = gen_server:call(admin_serv, {subscribe, {channel, Channel}}),
-
-    case cowboy_req:method(Req2) of
-        <<"POST">> ->
-            {{true, Channel}, Req2, State};
-        <<"PUT">> ->
-            {{true, Channel}, Req2, State};
-        _ ->
-            {true, Req2, State}
-    end.
 
 channel_to_json(Req, {index, Channels}) ->
     Body = jsx:encode(Channels),
